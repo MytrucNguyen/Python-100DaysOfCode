ca61e1eccaa719785f67635783c28e8e5d4f4fec
Mytruc Nguyen <80004058+MytrucNguyen@users.noreply.github.com>
2022-06-27 14:00:41 -0700

    Day 1 Completed: Variables, Printing, Inputs, and Debugging

---
+++ Day_001/Projects/main.py
@@ -0,0 +1,7 @@
+print("Welcome to the Band Name Generator.")
+
+city = input("What's the name of the city you grew up in? ")
+print(city)
+animal = input("What is your favorite animal? ")
+print(animal)
+print(f'Your band name could be {city} {animal}')
