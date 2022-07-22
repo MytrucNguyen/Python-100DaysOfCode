ed827ba5554b660ff2b45131f10c6e184214a0ed
Mytruc Nguyen <80004058+MytrucNguyen@users.noreply.github.com>
2022-07-21 20:14:21 -0700

    Day 25 - DataFrame and Series

--- Day_025/Notes/Reading_CSV/main.py
+++ Day_025/Notes/Reading_CSV/main.py
@@ -1,4 +1,5 @@
 import os
+from sqlite3 import DataError
 os.chdir(r"C:\Users\mytru\Documents\GitHub\Python-100DaysOfCode\Day_025\Notes\Reading_CSV")
 
 import csv
@@ -15,3 +16,36 @@ with open("weather_data.csv") as data_file:
 # python
 data = pandas.read_csv("weather_data.csv")
 print(data["temp"])
+
+data_dict = data.to_dict()
+print(data_dict)
+
+temp_list = data["temp"].to_list()
+print(temp_list)
+
+# average 
+# avg = sum(temp_list) / len(temp_list)
+# print(avg)
+print(data["temp"].mean())
+max_temp = data.temp.max()
+
+# Get Data in Row
+print(data[data.day == "Monday"])
+print(data[data.temp == max_temp])
+
+monday = data[data.day == "Monday"]
+print(monday.condition)
+
+monday_temp = int(monday.temp)
+monday_temp_F = monday_temp* 9/5 + 32
+print(monday_temp_F)
+
+# Creating from Scratch
+data_dict = {
+    "student": ["Amy", "James", "Angela"],
+    "scores": [76, 56, 65]
+}
+
+data = pandas.DataFrame(data_dict)
+print(DataError)
+data.to_csv("new_data.csv")
