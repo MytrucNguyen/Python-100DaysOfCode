e5f2e819cc64608d697eb9a5751867026e773314
Mytruc Nguyen <80004058+MytrucNguyen@users.noreply.github.com>
2022-07-21 18:01:08 -0700

    Reading CSV Data

---
+++ Day_025/main.py
@@ -0,0 +1,17 @@
+import os
+os.chdir(r"C:\Users\mytru\Documents\GitHub\Python-100DaysOfCode\Day_025")
+
+import csv
+import pandas
+with open("weather_data.csv") as data_file:
+    data = csv.reader(data_file)
+    temperatures = []
+
+    for row in data:
+        if row[1] != "temp": 
+            temperatures.append(int(row[1]))
+    print(temperatures)
+
+# python
+data = pandas.read_csv("weather_data.csv")
+print(data["temp"])
