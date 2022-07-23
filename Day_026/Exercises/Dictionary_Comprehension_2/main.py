6c6c7a8bec3130d8ec9e8bbae9ec9a379ef9e8e8
Mytruc Nguyen <80004058+MytrucNguyen@users.noreply.github.com>
2022-07-22 23:09:24 -0700

    Day 26 - Dictionary Comprehension 2

--- Day_026/Exercises/Dictionary_Comprehension_2/main.py
+++ Day_026/Exercises/Dictionary_Comprehension_2/main.py
@@ -1,9 +1,19 @@
-sentence = "What is the Airspeed Velocity of an Unladen Swallow?"
-# Don't change code above 👆
+weather_c = {
+    "Monday": 12,
+    "Tuesday": 14,
+    "Wednesday": 15,
+    "Thursday": 14,
+    "Friday": 21,
+    "Saturday": 22,
+    "Sunday": 24,
+}
+# 🚨 Don't change code above 👆
 
-# Write your code below:
-result = {word: len(word) for word in sentence.split()}
+
+# Write your code 👇 below:
+weather_f = {day: temp  * 9/5 + 32 for (day, temp) in weather_c.items()}
 
 
-print(result)
+print(weather_f)
+
 
