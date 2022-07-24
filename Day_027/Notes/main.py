8dfd3e7f37f51e365ef1a9e97bdf6c4c4274d807
Mytruc Nguyen <80004058+MytrucNguyen@users.noreply.github.com>
2022-07-24 14:40:44 -0700

    Day 27 - **kwargs

--- Day_027/Notes/main.py
+++ Day_027/Notes/main.py
@@ -26,3 +26,27 @@ def add(*args):
     return total
 
 print(add(3, 5, 6))
+
+# *kwargs
+def calculate(n, **kwargs):
+    print(kwargs)
+    # for key, value in kwargs.items():
+    #     print(key)
+    #     print(value)
+
+    # print(kwargs["add"])
+    n += kwargs["add"]
+    n *= kwargs["mutiply"]
+    print('n', n)
+
+calculate(2, add=3, mutiply=5)
+
+class Car:
+    def __init__(self, **kw):
+        self.make = kw.get("make")
+        self.model = kw.get("model")
+        self.color = kw.get("color")
+        self.seats = kw.get("seats")
+
+my_car = Car(make="Nissan")
+print(my_car.model)
