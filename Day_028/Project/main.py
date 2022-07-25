88be6b4fa5ec37312c4773616dabbbb53804c1b7
Mytruc Nguyen <80004058+MytrucNguyen@users.noreply.github.com>
2022-07-25 12:56:43 -0700

    Canvas Widget and Add Image to Tkinter

--- Day_028/Project/main.py
+++ Day_028/Project/main.py
@@ -1,3 +1,7 @@
+import tkinter
+from tkinter import *
+import os
+os.chdir(r"C:\Users\mytru\Documents\GitHub\Python-100DaysOfCode\Day_028\Project")
 
 # ---------------------------- CONSTANTS ------------------------------- #
 PINK = "#e2979c"
@@ -16,3 +20,14 @@ LONG_BREAK_MIN = 20
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
 
 # ---------------------------- UI SETUP ------------------------------- #
+window = Tk()
+window.title("Pomodoro")
+window.config(padx=100, pady=50, bg=YELLOW)
+
+canvas = Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
+tomato_img = PhotoImage(file="tomato.png")
+canvas.create_image(100, 112, image=tomato_img)
+canvas.create_text(103, 130, text="00:00", fill="white", font=(FONT_NAME, 25, "bold"))
+canvas.pack()
+
+window.mainloop()
