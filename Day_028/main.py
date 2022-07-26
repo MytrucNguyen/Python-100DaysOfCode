cd0ec03fe7f0e052f1296f1f75ba3d0bf9a816a6
Mytruc Nguyen <80004058+MytrucNguyen@users.noreply.github.com>
2022-07-26 08:00:16 -0700

    Day 28 - Pomodoro

--- Day_028/main.py
+++ Day_028/main.py
@@ -1,9 +1,5 @@
-import math
-import tkinter
 from tkinter import *
-import os
-os.chdir(r"C:\Users\mytru\Documents\GitHub\Python-100DaysOfCode\Day_028\Project")
-
+import math
 # ---------------------------- CONSTANTS ------------------------------- #
 PINK = "#e2979c"
 RED = "#e7305b"
@@ -16,8 +12,7 @@ LONG_BREAK_MIN = 20
 reps = 0
 timer = None
 
-# ---------------------------- TIMER RESET ------------------------------- #
-
+# ---------------------------- TIMER RESET ------------------------------- # 
 
 def reset_timer():
     window.after_cancel(timer)
@@ -28,7 +23,7 @@ def reset_timer():
     reps = 0
 
 
-# ---------------------------- TIMER MECHANISM ------------------------------- #
+# ---------------------------- TIMER MECHANISM ------------------------------- # 
 
 def start_timer():
     global reps
@@ -82,8 +77,7 @@ title_label.grid(column=1, row=0)
 canvas = Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
 tomato_img = PhotoImage(file="tomato.png")
 canvas.create_image(100, 112, image=tomato_img)
-timer_text = canvas.create_text(
-    100, 130, text="00:00", fill="white", font=(FONT_NAME, 35, "bold"))
+timer_text = canvas.create_text(100, 130, text="00:00", fill="white", font=(FONT_NAME, 35, "bold"))
 canvas.grid(column=1, row=1)
 
 start_button = Button(text="Start", highlightthickness=0, command=start_timer)
@@ -96,4 +90,19 @@ check_marks = Label(fg=GREEN, bg=YELLOW)
 check_marks.grid(column=1, row=3)
 
 
+
+
+
+
 window.mainloop()
+
+
+
+
+
+
+
+
+
+
+
