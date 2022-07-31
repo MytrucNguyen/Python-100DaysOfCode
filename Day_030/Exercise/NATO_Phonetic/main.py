75a5b60ad33853755c3f0403317998e78fc9cc34
Mytruc Nguyen <80004058+MytrucNguyen@users.noreply.github.com>
2022-07-31 16:39:59 -0700

    Day 30 - NATO Phonetic

---
+++ Day_030/Exercise/NATO_Phonetic/main.py
@@ -0,0 +1,20 @@
+import os
+os.chdir(r"C:\Users\mytru\Documents\GitHub\Python-100DaysOfCode\Day_030\Exercise\NATO_Phonetic")
+import pandas
+
+data = pandas.read_csv("nato_phonetic_alphabet.csv")
+
+phonetic_dict = {row.letter: row.code for (index, row) in data.iterrows()}
+print(phonetic_dict)
+
+def generate_phonetic():
+    word = input("Enter a word: ").upper()
+    try:
+        output_list = [phonetic_dict[letter] for letter in word]
+    except KeyError:
+        print("Sorry, only letters in the alphabet please.")
+        generate_phonetic()
+    else:
+        print(output_list)
+
+generate_phonetic()
