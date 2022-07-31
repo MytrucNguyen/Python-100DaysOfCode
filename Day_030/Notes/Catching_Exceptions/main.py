550a5ab3c2cd5e8c49463919ad914a912403253e
Mytruc Nguyen <80004058+MytrucNguyen@users.noreply.github.com>
2022-07-31 09:42:42 -0700

    Day 30 - Raising own Exceptions

--- Day_030/Notes/Catching_Exceptions/main.py
+++ Day_030/Notes/Catching_Exceptions/main.py
@@ -1,4 +1,5 @@
 import os
+from typing import Type
 os.chdir(r"C:\Users\mytru\Documents\GitHub\Python-100DaysOfCode\Day_030\Notes\Catching_Exceptions")
 
 #FileNotFound
@@ -14,22 +15,32 @@ os.chdir(r"C:\Users\mytru\Documents\GitHub\Python-100DaysOfCode\Day_030\Notes\Ca
 # finally:
     # do this no matter what happens
 
-try:
-    file = open("a_file.txt")
-    a_dictionary  = {"key": "value"}
-    print(a_dictionary["key"])
+# try:
+#     file = open("a_file.txt")
+#     a_dictionary  = {"key": "value"}
+#     print(a_dictionary["key"])
 
-except FileNotFoundError:
-    file = open("a_file.txt", "w")
-    file.write("something")
+# except FileNotFoundError:
+#     file = open("a_file.txt", "w")
+#     file.write("something")
 
-except KeyError as error_message:
-    print(f"That key {error_message} does not exist")
+# except KeyError as error_message:
+#     print(f"That key {error_message} does not exist")
 
-else:
-    content = file.read()
-    print(content)
+# else:
+#     content = file.read()
+#     print(content)
 
-finally:
-    file.close()
-    print("File closed")
+# finally:
+#     raise TypeError("This is an errror that I made up")
+#     # file.close()
+#     # print("File closed")
+
+height = float(input("Height: "))
+weight = float(input("Weight: "))
+
+if height > 3:
+    raise ValueError("Human Height should not be over 3 meters.")
+
+bmi = weight / height ** 2
+print(bmi)
